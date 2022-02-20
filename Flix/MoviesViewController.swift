cd6511c2cc118f171bd6e65b803d9a002434f346
Ji Wang <wj42421999@gmail.com>
2022-02-20 00:18:50 -0500

    Add LaunchScreen View with app's icon and title

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -8,28 +8,49 @@
 import UIKit
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet var tableView: UITableView!
     
-    
-    
-    
-    
-    @IBOutlet weak var tableView: UITableView!
+    private var movies = [[String: Any]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.dataSource = self
         tableView.delegate = self
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { data, _, error in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+
+                // TODO: Get the array of movies
+                self.movies = dataDictionary["results"] as! [[String: Any]]
+                self.tableView.reloadData()
+
+                // TODO: Store the movies in a property to use elsewhere
+                // TODO: Reload your table view data
+            }
+        }
+        task.resume()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        return movies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         
-        cell.textLabel?.text = "row : \(indexPath.row)"
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        print(title)
+        
+        cell.textLabel!.text = title
         
         return cell
     }
