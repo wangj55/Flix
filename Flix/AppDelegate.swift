877cd3a5ac8ee148c78933e6b5d3076e8e286ba5
Ji Wang <wj42421999@gmail.com>
2022-02-20 01:57:26 -0500

    Leave a fixed 0.1s for the launch screen to display

--- Flix/AppDelegate.swift
+++ Flix/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        usleep(useconds_t(100_000)) // sleep 0.1s, show the launch screen
         return true
     }
 
