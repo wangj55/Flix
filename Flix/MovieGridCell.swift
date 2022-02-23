e34d6abfc639b64d3f3dcb9d2f2ca61df33a8329
Ji Wang <wj42421999@gmail.com>
2022-02-22 23:30:30 -0500

    Add Tab Bar and a superhero page

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Ji Wang on 2/22/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
